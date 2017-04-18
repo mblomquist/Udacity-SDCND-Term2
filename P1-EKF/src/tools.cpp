979d5a260cfe3a935ec73ef443ece8a7f5cd1940
Matt <blomquist.matt@gmail.com>
2017-04-17 19:21:05 -0700

    fix: correct calculate jacobian.

--- P1-EKF/src/tools.cpp
+++ P1-EKF/src/tools.cpp
@@ -54,17 +54,22 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
     * Calculate a Jacobian here.
   */
 	  MatrixXd Hj(3, 4);
-
 	  //recover state parameters
-	  double px = x_state(0);
-	  double py = x_state(1);
-	  double vx = x_state(2);
-	  double vy = x_state(3);
+	  float px = x_state(0);
+	  float py = x_state(1);
+	  float vx = x_state(2);
+	  float vy = x_state(3);
 
 	  //pre-compute a set of terms to avoid repeated calculation
-	  double c1 = px*px + py*py;
-	  double c2 = sqrt(c1);
-	  double c3 = (c1*c2);
+	  float c1 = px*px + py*py;
+	  float c2 = sqrt(c1);
+	  float c3 = (c1*c2);
+
+	  //check division by zero
+	  if (fabs(c1) < 0.0001) {
+			cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+			return Hj;
+	  }
 
 	  //compute the Jacobian matrix
 	  Hj << (px / c2), (py / c2), 0, 0,

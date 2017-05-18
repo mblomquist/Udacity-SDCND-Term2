57072526be07b6d34810f20c6ca4471ec8b04e2c
Matt Blomquist <blomquist.matt@gmail.com>
2017-05-18 08:44:38 -0700

    Update particle_filter.cpp
    fix: Add setAssociations and resample routines.

--- P3-PF/src/particle_filter.cpp
+++ P3-PF/src/particle_filter.cpp
@@ -86,6 +86,25 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
 	//   implement this method and use it as a helper during the updateWeights phase.
 
+      for (LandmarkObs &obs : observations) {
+
+            double min_distance = 0.000001;
+
+            for (int i = 0; i < predicted.size(); i++) {
+
+                  if (predicted[i].id == 0) {
+
+                        const double d = dist(obs.x, obs.y, predicted[i].x, predicted[i].y);
+
+                        if (d < min_distance) {
+                              obs.id = i;
+                              min_distance = d;
+                        }
+                  }
+            }
+      }
+
+      return;
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
@@ -101,6 +120,75 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33. Note that you'll need to switch the minus sign in that equation to a plus to account 
 	//   for the fact that the map's y-axis actually points downwards.)
 	//   http://planning.cs.uiuc.edu/node99.html
+
+      weights.clear();
+      weights.reserve(num_particles);
+
+      double std_x = std_landmark[0];
+      double std_y = std_landmark[1];
+
+      if (std_x < 0.001 || std_y < 0.001) {
+            double c = 1;
+      }
+      else {
+            double c = 0.5 / (M_PI*std_x*std_y);
+      }
+
+      for (Particle &p : particles)
+      {
+            std::vector<LandmarkObs> landmarks_in_sensor_range;
+            std::vector<LandmarkObs> observations_in_map_coords;
+            observations_in_map_coords.reserve(observations.size());
+
+            // transform observations into map coordinates using the perspective of the current particle
+            for (const LandmarkObs &obs : observations)
+            {
+                  LandmarkObs transformed_obs;
+
+                  transformed_obs.id = 0;
+                  transformed_obs.x = p.x + obs.x * cos(p.theta) - obs.y * sin(p.theta);
+                  transformed_obs.y = p.y + obs.x * sin(p.theta) + obs.y * cos(p.theta);
+
+                  observations_in_map_coords.push_back(transformed_obs);
+            }
+
+            // find landmarks in the map which are within sensor_range of the current particle
+            for (const Map::single_landmark_s &lm : map_landmarks.landmark_list)
+            {
+                  if (dist(p.x, p.y, lm.x_f, lm.y_f) <= sensor_range)
+                  {
+                        LandmarkObs lm_tmp = { lm.id_i, lm.x_f, lm.y_f };
+                        landmarks_in_sensor_range.push_back(lm_tmp);
+                  }
+            }
+
+            // associate in-range landmarks with transformed sensor readings
+            dataAssociation(observations_in_map_coords, landmarks_in_sensor_range);
+
+            // calculate the weight of the particle
+            double prob = 1.0;
+
+            for (const LandmarkObs &lm : landmarks_in_sensor_range)
+            {
+                  const LandmarkObs &closest_obs = observations_in_map_coords[lm.id];
+
+                  const double x_diff = pow((closest_obs.x - lm.x) / std_x, 2),
+                        y_diff = pow((closest_obs.y - lm.y) / std_y, 2);
+
+                  prob *= c * exp(-0.5 * (x_diff + y_diff));
+
+                  // Don't let a particle have zero probability of being chosen
+                  if (prob < 1e-4)
+                        break;
+            }
+
+            // store the probability of this particle being real in the weight member and the weights_ vector.
+            p.weight = prob;
+            weights.push_back(prob);
+      } // end particle loop
+
+      return;
+            
 }
 
 void ParticleFilter::resample() {
@@ -108,6 +196,21 @@ void ParticleFilter::resample() {
 	// NOTE: You may find std::discrete_distribution helpful here.
 	//   http://en.cppreference.com/w/cpp/numeric/random/discrete_distribution
 
+    // Create random index generator where the probability of each particle index to be selected
+    // is equivalent to its weight.
+      std::default_random_engine gen;
+      std::discrete_distribution<int> d(weights.begin(), weights.end());
+
+      // temporary list to store re-sampled particles
+      std::vector<Particle> resampled_particles;
+      resampled_particles.reserve(num_particles);
+
+      // generate a random index and append the corresponding particle to the re-sampling list
+      for (int i = 0; i < num_particles; ++i)
+            resampled_particles.push_back(particles[d(gen)]);
+
+      // replace the old particles with the re-sampled particles
+      particles = resampled_particles;
 }
 
 Particle ParticleFilter::SetAssociations(Particle particle, std::vector<int> associations, std::vector<double> sense_x, std::vector<double> sense_y)

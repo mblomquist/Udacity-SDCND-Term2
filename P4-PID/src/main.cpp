a325a8c2158eb9b7166ef11ade57e56cd8355f51
Matt Blomquist <blomquist.matt@gmail.com>
2017-06-05 10:14:25 -0700

    Update main.cpp
    fix: update PID values.

--- P4-PID/src/main.cpp
+++ P4-PID/src/main.cpp
@@ -36,8 +36,8 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
-  pid.Init(1, 0, 0);
+  // TODO: Initialize the pid variable. (Initial guess taken from slack)
+  pid.Init(0.1, 3.0, 0.0001);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
